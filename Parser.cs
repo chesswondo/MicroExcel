aaf4cdc402db53e0e05a51525244b7d7d5120e10
Anton Krasikov <chesswondo@gmail.com>
2022-10-05 16:15:29 +0300

    Підправлено вивід інформації про помилку

--- Parser.cs
+++ Parser.cs
@@ -20,13 +20,13 @@ namespace MicroExcel
         public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string
         msg, RecognitionException e)
         {
-            throw new ArgumentException("Invalid Expression: {0}", msg, e);
+            throw new ArgumentException("Invalid Expression: " + msg, e);
         }
         //IAntlrErrorListener&lt;int&gt; implementation
         public void SyntaxError(IRecognizer recognizer, int offendingSymbol, int line, int charPositionInLine, string msg,
         RecognitionException e)
         {
-            throw new ArgumentException("Invalid Expression: {0}", msg, e);
+            throw new ArgumentException("Invalid Expression: " + msg, e);
         }
     }
 
