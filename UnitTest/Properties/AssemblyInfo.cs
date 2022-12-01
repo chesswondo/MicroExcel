b6c6632749120fd1004b8457bbb9e0eafb31531b
Anton Krasikov <chesswondo@gmail.com>
2022-12-01 21:01:28 +0200

    UnitTest

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("55c3db5b-2b43-4e0a-b209-4f8be89fddf4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTestClass.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MicroExcel;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestClass
+    {
+        static void ZeroUpZero()
+        {
+            MEParser p = new MEParser();
+            p.Evaluate("0^0", null);
+        }
+        static void Wrong1()
+        {
+            MEParser p = new MEParser();
+            p.Evaluate("5/0", null);
+        }
+        static void Wrong2()
+        {
+            MEParser p = new MEParser();
+            p.Evaluate("abc+3", null);
+        }
+
+        [TestMethod]
+        public void TestParserArithmetic()
+        {
+            MEParser p = new MEParser();
+            Assert.AreEqual(-3, p.Evaluate("2+3-8", null));
+            Assert.AreEqual(10, p.Evaluate("2*3+8/2", null));
+        }
+
+        [TestMethod]
+        public void TestParserWrong()
+        {
+            MEParser p = new MEParser();
+            Assert.ThrowsException<ArgumentException>(ZeroUpZero);
+            Assert.ThrowsException<ArgumentException>(Wrong1);
+            Assert.ThrowsException<ArgumentException>(Wrong2);
+        }
+    }
+}
